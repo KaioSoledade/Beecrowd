2b1ba8d977b9b8ae1da9ce1a4a37a8b9d0dcbf28
Kaio Soledade <soledadekaio@gmail.com>
2023-09-20 23:43:33 -0300

    Desenvolvimento

---
+++ 1040.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+char frase1[1000];
+char frase2[1000];
+char frase3[1000];
+
+   
+    scanf("%c", &frase1);
+    scanf("%c", &frase2);
+    scanf("%c", &frase3);
+   
+    if(frase1[1] == frase2[1] = frase3[1]){
+    printf("S");
+}if else(frase){
+printf("N");
+}
+
+}
