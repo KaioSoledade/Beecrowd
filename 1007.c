d04517a65f4212376f37ec64583cdd41587c12a4
Kaio Soledade <soledadekaio@gmail.com>
2023-09-21 23:22:30 -0300

    BEECROWD

---
+++ 1007.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+int A, B, C,D;
+
+scanf("%d\n", &A);
+scanf("%d\n", &B);
+scanf("%d\n", &C);
+scanf("%d\n", &D);
+
+
+printf("DIFERENCA = %d\n", (A * B - C * D));
+    return 0;
+   
+}
