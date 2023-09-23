7d90d7fe5eae675ec136fa40510504ad8f1fd0c0
Kaio Soledade <soledadekaio@gmail.com>
2023-09-23 20:34:30 -0300

    BEECROWD

---
+++ 1008.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+double A, B, C;
+
+scanf("%lf\n", &A);
+scanf("%lf\n", &B);
+scanf("%lf\n", &C);
+
+
+printf("NUMBER = %.0lf\n", A);
+printf("SALARY = U$ %.2lf\n", B * C);
+
+    return 0;
+   
+}
