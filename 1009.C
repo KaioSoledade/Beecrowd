1f62f8e15e0917e88dc47aea53edf14f12e6c654
Kaio Soledade <soledadekaio@gmail.com>
2023-09-27 10:04:55 -0300

    BEECROWD

---
+++ 1009.C
@@ -0,0 +1,18 @@
+                 
+#include <stdio.h>
+
+int main()
+{
+    char name[30];
+    double salario, venda ,conta, TOTAL;
+    
+    scanf("%s ", &name);
+    scanf("%lf", &salario);
+    scanf("%lf", &venda);
+
+    
+    conta = (venda*15)/100;
+    TOTAL = conta + salario;
+    
+    printf("TOTAL = R$ %.2lf\n", TOTAL);
+}
