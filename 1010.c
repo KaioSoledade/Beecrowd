d46dbce7cbaa553e863f4f2427ebbc3a00ab20dd
Kaio Soledade <soledadekaio@gmail.com>
2023-09-28 23:38:42 -0300

    BEECROWD

---
+++ 1010.c
@@ -0,0 +1,22 @@
+                 
+#include <stdio.h>
+
+int main()
+{
+    int uni1, uni2, quant1, quant2;
+    double preco1, preco2, TOTAL;
+    
+    scanf("%d", &uni1);
+    scanf("%d", &quant1);
+    scanf("%lf", &preco1);
+
+
+    scanf("%d", &uni2);
+    scanf("%d", &quant2);
+    scanf("%lf", &preco2);
+
+    
+    TOTAL = quant1*preco1 + quant2*preco2;
+    
+    printf("VALOR A PAGAR: R$ %.2lf\n", TOTAL);
+}
