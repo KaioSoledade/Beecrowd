3fe3a6cabdb81cd594084c83631a96b52bbc7713
Kaio Soledade <soledadekaio@gmail.com>
2023-10-14 12:05:33 -0300

    BEECROWD

---
+++ Aula/switch.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int main(){
+
+    int x = 2;
+    switch (x){
+
+    case 2:
+    printf("Caso %d\n", x);
+
+        break;
+    
+    default:
+        break;
+    }
+
+    char y = 'a';
+    switch (y){
+
+    case 97:
+    printf("Caso %c\n", y);
+        break;
+    
+    default:
+        break;
+    }
+}
+
