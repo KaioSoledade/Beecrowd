c75d45c94dff5359a31f6760358a3d37699174ca
Kaio Soledade <soledadekaio@gmail.com>
2024-03-05 23:19:56 -0300

    Algoritmos

---
+++ Algoritmos-Aula/Resume-sobre-funcao.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+double funcao( "Dados que seu codigo precisa, como: Float A / Float B / Float C" ){
+//^-- Tipo que a função retornara
+    dado = A + B + C;
+    return dado;
+//           ^--- O que sera retornado para a função main()
+}
+int main()
+{
+
+    double Resultado;
+   
+    Resultado = funcao( "Dados que seu codigo vai oferecer, como: A / B / C" )
+//     ^--Aonde serão armazenados
+    return 0;
+}
